459206738c0496648d81cc52e4affbbb93805020
bxrbxrx <barbaracalle10i3@gmail.com>
2021-02-15 19:18:56 -0500

    Closuuuuure

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,11 @@
+function welcomeMessage(fullName){
+  function message(){
+    let mssg = "Welcome"
+    alert(mssg + ' ' + fullName);
+  }
+  return message;
+}
+
+let guillaume = welcomeMessage('Guillaume');
+let alex = welcomeMessage('Alex');
+let fred = welcomeMessage('Fred');
