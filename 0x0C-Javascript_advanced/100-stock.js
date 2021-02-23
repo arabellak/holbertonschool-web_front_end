476119b1caae0a9c009a42ae877788a14970066b
arabellak <barbaracalle10i3@gmail.com>
2021-02-23 15:16:15 -0500

    100

---
+++ 0x0C-Javascript_advanced/100-stock.js
@@ -0,0 +1,18 @@
+let stock = {
+    macbook: '2',
+    iphone: '4'
+};
+
+function processPayment(itemName) {
+
+    console.log('Payment is being processed for item', itemName);
+};
+
+function processError(itemName) {
+    console.log('No more', itemName, 'in stock');
+    console.log('Payment is not being processed');
+};
+
+function processOrder(itemName, callbackPayment, callbackError) {
+    console.log('Verifying the stock of', itemName)
+};
